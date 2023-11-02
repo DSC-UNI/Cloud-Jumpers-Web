e933ef5bfafceb383c44ebb73ccc59c296b6af34
Jose Guerra <jose.guerra.m@uni.pe>
2023-11-01 22:22:22 -0500

    added backend
    api del ost del form

--- web/src/app/components/crear-historia/crear-historia.component.ts
+++ web/src/app/components/crear-historia/crear-historia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-crear-historia',
@@ -10,33 +11,46 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CrearHistoriaComponent implements OnInit {
   historiaForm: FormGroup;
 
-  constructor() {
-    this.historiaForm = new FormGroup({
+  formData: any = {};
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.historiaForm = this.fb.group({
       dni: new FormControl('', [
         Validators.required,
         Validators.pattern('^[0-9]{8}$')
       ]),
-      fecha: new FormControl(null, Validators.required),
-
-      pregunta1: new FormControl(null, Validators.required),
-      pregunta2: new FormControl('', Validators.required),
-      pregunta3: new FormControl('', Validators.required),
-      pregunta4: new FormControl('', Validators.required),
-      pregunta5: new FormControl('', Validators.required),
-      pregunta6: new FormControl('', Validators.required),
+      created_at: new FormControl(null, Validators.required),
+      antecedentes_patologicos: new FormControl(null, Validators.required),
+      antecedentes_quirurgicos: new FormControl('', Validators.required),
+      rams: new FormControl('', Validators.required),
+      relato_cronologico: new FormControl('', Validators.required),
+      plan: new FormControl('', Validators.required),
     });
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.historiaForm.valid) {
-      // Envía los datos del formulario
-      console.log(this.historiaForm.value);
-    } else {
-      // Muestra errores o realiza otras acciones
-      console.log('Formulario no válido. Por favor, complete todas las preguntas.');
+
+      this.formData = {
+        dni: this.historiaForm.get('dni')?.value,
+        created_at: this.historiaForm.get('created_at')?.value,
+        antecedentes_patologicos: this.historiaForm.get('antecedentes_patologicos')?.value,
+        antecedentes_quirurgicos: this.historiaForm.get('antecedentes_quirurgicos')?.value,
+        rams: this.historiaForm.get('rams')?.value,
+        relato_cronologico: this.historiaForm.get('relato_cronologico')?.value,
+        plan: this.historiaForm.get('plan')?.value,
+      }
+
+      try {
+        const response = await this.http.post("http://localhost:8000/api/projects/", this.formData).toPromise();
+        console.log("datos enviados con exito", response);
+      } catch (error) {
+        console.error("error al enviar los datos", error);
+      }
+
     }
   }
 }
--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -11,13 +11,49 @@ import { HttpClient } from '@angular/common/http';
 
 
 export class HomeComponent implements OnInit {
-  constructor(private http:HttpClient) { }
-  obj :any;
-  ngOnInit(): void {
-  this.obj = this.http.get("http://localhost:8000/api/projects/").subscribe(
-    data => this.obj = data
-  )
-  }
   
+  obj :any;
+
+  formData: any = {};
+
+  constructor(private http:HttpClient) { }
+
+  send_data = {
+    "add":"true",
+    "obj":{
+      "dni": "9999999",
+    "antecedentes_patologicos": "dgjsdfgjlsdfjgljdfs",
+    "antecedentes_quirurgicos": "wefaefaedgfsfd",
+    "rams": "pojgsfopjgsfdpgs",
+    "relato_cronologico": "pjoefjadfgjp",
+    "plan": "posdjgsdofjpgjpsdf",
+    }
+  }
+
+  ngOnInit(): void {
+    //método para llamar a las historias clínicas
+    this.getStories();
+  }
+
+  getStories() {
+    this.http.get("http://localhost:8000/api/projects/").subscribe(
+      (data: any) => {
+        this.obj = data;
+      }
+    );
+  }
+
+  postStories() {
+    this.http.post("http://localhost:8000/api/projects/", this.formData).subscribe(
+      (response: any) => {
+        console.log('Proyecto creado con éxito:', response);
+        // Puedes realizar acciones adicionales después del POST si es necesario
+        this.getStories(); // Actualiza la lista de proyectos después del POST
+      },
+      (error) => {
+        console.error('Error al crear el proyecto:', error);
+      }
+    );
+  }
 }
 
